8ca464d65a1f35fcc58ce6517629b3ded319c4a2
Samuel Moreno <samuel.moreno@crowdstrike.com>
2026-04-15 09:48:38 +0200

    feat: add description to pkg-b (#16)

--- packages/pkg-b/index.js
+++ packages/pkg-b/index.js
@@ -1,2 +1,2 @@
 const pkgA = require("@samuelmp/pkg-a");
-module.exports = { name: "pkg-b", version: "1.0.4", dependency: pkgA };
+module.exports = { name: "pkg-b", version: "1.0.4", description: "test tag-based release", dependency: pkgA };
